00f51602cd666d320e248276e04a242a4e0fa794
Miguel Galindo <miguelgalindo1214@gmail.com>
2023-09-16 21:05:46 -0400

    set up server using express that reads from files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static('./public'))
+
 app.get('/',(req, res)=>{
-    res.status(200).send('Home Page');
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
 })
 
 
-app.get('/about',(req, res)=>{
-    res.status(200).send('About Page');
-})
 
 app.all('*',(req, res)=>{
     res.status(404).send('<h1>Resource Not Found</h1>')
