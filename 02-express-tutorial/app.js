5dc3353990a7c2d994a0a59174bf5e5311713f8b
Miguel Galindo <miguelgalindo1214@gmail.com>
2023-09-16 20:11:29 -0400

    creating a node server and reading files from the file system

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,29 @@
-console.log('Express Tutorial')
+const http = require('http');
+const { readFileSync, readFile } = require('fs');
+
+//get all files
+
+const homePage = readFileSync('./navbar-app/index.html');
+const server = http.createServer((req, res)=>{
+
+    const URL = req.url;
+    if(URL === '/'){
+        res.writeHead(200, {'content-type':'text/html'});
+        res.end(homePage);
+    } else if (URL === '/about') {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.end('<h1>About Page</h1>');
+    } else {
+        res.writeHead(400, {'content-type':'text/html'});
+        res.write('<h1>Page not found</h1>');
+        res.end();
+    }
+
+
+
+})
+
+
+
+server.listen(8000);
+
