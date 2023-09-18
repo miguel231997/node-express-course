bf9e9dde13948d04185d74cffcde68c31dc4ec75
Miguel Galindo <miguelgalindo1214@gmail.com>
2023-09-17 20:09:42 -0400

    Reading in JSON data to the server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,36 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
 
-app.use(express.static('./public'))
+const { products } = require('./data.js')
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
-})
-
-
-
-app.all('*',(req, res)=>{
-    res.status(404).send('<h1>Resource Not Found</h1>')
-})
-
-app.listen(8000,()=>{
-    console.log('server is listening on port 8000...');
+    res.send('<h1> Home Page</h1><a href="/api/products">Products</a>');
 });
 
-//app.get
-//app.post
-//app.put
-//app/delete
+app.get('/api/products',(req, res)=>{
+    const newProducts = products.map((product)=>{
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+
+    res.json(newProducts);
+})
+
+app.get('/api/products/:productID', (req, res)=>{
+    const { productID } = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID))
+    if(!singleProduct){
+        return res.status(404).send('Product Does Not Exist');
+    }
+    res.json(singleProduct)
+})
+
+app.listen(8000, ()=>{
+    console.log('Server is running on Port 8000....');
+})
+
+
+
 
