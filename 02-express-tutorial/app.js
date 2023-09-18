65fca39935d608199e06481bbbb72935bc58911a
Miguel Galindo <miguelgalindo1214@gmail.com>
2023-09-17 21:44:01 -0400

    adding MiddleWare

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,54 +2,16 @@ const express = require('express');
 
 const app = express();
 
-const { products } = require('./data.js')
+const logger = require('./logger.js');
 
-app.get('/',(req, res)=>{
-    res.send('<h1> Home Page</h1><a href="/api/products">Products</a>');
-});
+app.use(logger);
 
-app.get('/api/products',(req, res)=>{
-    const newProducts = products.map((product)=>{
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
+//req => middleware => res
 
-    res.json(newProducts);
+app.get('/', (req, res)=>{
+    res.send('Home')
 })
 
-app.get('/api/products/:productID', (req, res)=>{
-    const { productID } = req.params;
-
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct){
-        return res.status(404).send('Product Does Not Exist');
-    }
-    res.json(singleProduct)
-})
-
-app.get('/api/vi/query', (req, res) => {
-
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-
-    if(search) {
-        sortedProducts = sortedProducts.filter((product) =>{
-            return product.name.startsWith(search);
-        })
-    }
-
-    if(limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-
-    if(sortedProducts.length < 1) {
-        //res.status(200).send('No Products Matched');
-        return res.status(200).json({ success: true, data: []})
-    }
-
-    res.status(200).json(sortedProducts);
-
-})
 
 app.listen(8000, ()=>{
     console.log('Server is running on Port 8000....');
